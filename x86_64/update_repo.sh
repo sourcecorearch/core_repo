ce5918869eda3644a62d7055051924c28d318796
sourcecorearch <sourcecorearch@gmail.com>
2026-02-22 09:54:31 -0300

    update for core linux

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-rm core_repo.*
+set -e
 
-echo "repo-add"
+echo "🧹 Cleaning old repo files..."
+rm -f core_repo.db core_repo.db.tar.gz core_repo.files core_repo.files.tar.gz
 
+echo "📦 Running repo-add..."
 repo-add -n -R core_repo.db.tar.gz *.pkg.tar.zst
-sleep 2
+
+echo "🔁 Fixing GitHub symlink issue..."
+rm -f core_repo.db core_repo.files
+cp core_repo.db.tar.gz core_repo.db
+cp core_repo.files.tar.gz core_repo.files
 
 echo "#####"
 echo "Repo Update!!"
